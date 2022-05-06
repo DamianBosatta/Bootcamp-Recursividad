a266d8a30608443bfc5d9c544e9cc9c57e687b89
Damian <103905478+DamianBosatta@users.noreply.github.com>
2022-05-05 22:42:04 -0300

    Recursividad

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace _5deMayo
+//{
+//    class Program
+//    {
+
+//        #region Recursividad //// ejercicio 7 Recursividad
+//        //static void Main(string[] args)
+//        //{
+//        //    List<int> entero = new List<int>();
+//        //    Random rnd = new Random();
+
+
+//        //    for (int i = 0; i < 5; i++)
+//        //    {
+
+//        //        entero.Add(rnd.Next(0, 1000));
+
+
+//        //    }
+
+//        //    foreach(int i in entero)
+//        //    {
+
+//        //        Console.WriteLine(" la posision : " + entero.Count + " Vale : " + i);
+
+
+
+
+//        //    }
+
+
+//        //    Console.WriteLine(" El  total de la lista recurisva es : " + totalLista(entero,0,entero.Count-1));
+
+
+//        //    Console.ReadKey();
+
+
+
+//        //     int totalLista(List<int> lis,int sumaTotal,int puntero)
+//        //    {
+//        //        if (puntero >= 0)
+//        //        {
+
+
+
+//        //            sumaTotal = lis[puntero] + totalLista(lis,sumaTotal,puntero-1);
+//        //        }
+
+
+
+
+
+
+//        //        return sumaTotal;
+//        //    }
+
+
+
+
+
+
+//        //}
+//        #endregion
+//        //        11- A fin de año se tiene la lista de alumnos de un curso(no más de 40 alumnos), con la información de cada uno:
+//        //        - Nombre(hasta 20 caracteres) - Nota parcial 1 - Nota parcial 2 reales - Nota parcial 3 Se desea saber toda la información de los 4 mejores alumnos de dicha comisión.Exhibir además,
+//        //        la lista de los nombres de los alumnos ordenada en forma decreciente de acuerdo a la última nota.
+
+//        //OBSERVACION: Se utilizará el promedio de notas para determinar los 4 mejores alumnos
+
+       
+//        static void Main(string[] args)
+//        {
+
+           
+
+
+
+
+
+
+
+
+//        }
+
+
+
+
+
+
+//    }
+//}
